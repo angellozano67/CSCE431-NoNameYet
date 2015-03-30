76cded44eef83d5f515ac88d0d317117943eef8e
Thomas Scott Jasper <scojasp@gmail.com>
2015-03-30 18:04:10 -0500

    Testing controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  def index
+    
+  end
+  def about
+    
+  end
+  def contact
+    
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
