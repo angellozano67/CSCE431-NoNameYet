3ecf1fa9894cf1b8fb7715b3c238711c48b42cba
Scott Jasper <scojasp@gmail.com>
2015-04-16 19:01:10 -0500

    Adding temp homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'index' => 'public#indexTEST'
   get 'signup' => 'users#new'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
