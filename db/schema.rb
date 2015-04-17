a1caf3e30434ec773c3e588baf334a6e80f87f23
Chris Murray <ccmurray93@gmail.com>
2015-04-16 19:18:11 -0500

    Changed Gemfile to ruby2.2 and db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150414022801) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
