43e466b7816f96a557b4a843a429f190e4752763
Scott Jasper <scojasp@gmail.com>
2015-04-30 16:35:39 -0500

    Finished ruby side of login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,4 +8,41 @@ class User < ActiveRecord::Base
 					  uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 2 }
+	
+	# Hash digest of string.
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+	
+	# Random Token
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+	
+	# Remember user in db for use in persistent sessions
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+	
+	# True if token matches digest
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+	
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+	class << self
+		def digest(string)
+			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+			BCrypt::Password.create(string, cost:cost)
+		end
+		
+		def new_token
+			SecureRandom.urlsafe_base64
+		end
+	end
 end
