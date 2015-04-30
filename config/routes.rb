41faea41dd35ede8a1460c9d25640cd843a63009
Scott Jasper <scojasp@gmail.com>
2015-04-30 18:01:27 -0500

    Removing 1st login entry in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     root               'application#index'
     get    'about'   => 'application#about'
     get    'contact' => 'application#contact'
-    get    'login'   => 'application#login'
+    # get    'login'   => 'application#login'
 	get    'signup'  => 'users#new'
 	get    'login'   => 'sessions#new'
 	post   'login'   => 'sessions#create'
