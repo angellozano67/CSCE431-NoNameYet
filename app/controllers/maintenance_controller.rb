8de6af77b57ecbbdbeb108985b6ad2607e33f921
mattbsox <mbowersox@tamu.edu>
2015-05-03 21:44:27 -0500

    Messing with redirect_to in the controller

--- app/controllers/maintenance_controller.rb
+++ app/controllers/maintenance_controller.rb
@@ -15,7 +15,7 @@ class MaintenanceController < ApplicationController
     @maintenance_forms = MaintenanceForm.new(maintenance_params)
     if @maintenance_forms.save
       flash[:success] = "Thank you for letting us know, we will address the issue."
-      redirect_to @maintenance_forms
+      redirect_to :back
     else
       render "new"
     end
