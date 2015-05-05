7a9203008470367e97d084e09398ad056a2073bf
Scott Jasper <scojasp@gmail.com>
2015-05-05 15:06:18 -0500

    Manually storing checkbox value

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,11 @@ class UsersController < ApplicationController
   def update
 	@user = User.find(params[:id])
 	if @user.update_attributes(user_params)
+		if user_params.verified == '1'
+			@user.verified = 'true'
+		else
+			@user.verified = 'false'
+		end
 		flash[:success] = "Profile updated."
 		redirect_to @user
 	else
