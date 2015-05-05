6e6f4fdb21d93e71e844724aa9adb2a21ec64c40
Scott Jasper <scojasp@gmail.com>
2015-05-04 22:22:41 -0500

    Trying something else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update, :show]
   before_action :admin_user, only: [:destroy]
 
   def index
@@ -97,7 +97,7 @@ class UsersController < ApplicationController
 	
 	def correct_user
 		@user = User.find(params[:id])
-		redirect_to(root_url) unless current_user?(@user)
+		redirect_to(root_url) unless (current_user?(@user) || current_user.isadmin?)
 	end
 	
 	def admin_user
