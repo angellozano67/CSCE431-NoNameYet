56aef5c18851a0491393faf39d3b36669e242c5e
mattbsox <mbowersox@tamu.edu>
2015-05-04 21:05:26 -0500

    more fiddling

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get    'login'   => 'application#login'
 	get    'signup'  => 'users#new'
 	get    'login'   => 'sessions#new'
-    get    'maintenance' => 'maintenance#index'
+    get    'maintenance_forms' => 'maintenance#index'
     get    'new_request' => 'maintenance#new'
 	post   'login'   => 'sessions#create'
 	delete 'logout'  => 'sessions#destroy'
