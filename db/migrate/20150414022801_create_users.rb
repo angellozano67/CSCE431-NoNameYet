b63f0f0e7a83526d338cdb3bbf2781f5819651ee
Scott Jasper <scojasp@gmail.com>
2015-05-05 19:59:18 -0500

    Adding a migration for users?

--- db/migrate/20150414022801_create_users.rb
+++ db/migrate/20150414022801_create_users.rb
@@ -1,10 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
       t.string :email
-
-      t.timestamps null: false
+	  t.boolean :verified, default: false
+	  t.boolean :isadmin, default: false
+      t.string :name
+	  t.string :password_digest
+	  t.string :remember_token
+	  t.string :remember_digest
+	  t.string :notes
+	  t.string :address
+	  t.string :organization
+	  t.string :insuranceprovider
+	  t.string :policynum
     end
+	
   end
 end
