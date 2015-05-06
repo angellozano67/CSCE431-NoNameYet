9dc4b28f54ec2f121fefcab88c4a4d7c34d31e5e
Chris Murray <ccmurray93@gmail.com>
2015-05-06 03:57:25 -0500

    Reservation logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414022801) do
+ActiveRecord::Schema.define(version: 20150506082415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "start_time"
+    t.string  "end_time"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string  "email"
@@ -31,4 +37,5 @@ ActiveRecord::Schema.define(version: 20150414022801) do
     t.string  "policynum"
   end
 
+  add_foreign_key "reservations", "users"
 end
